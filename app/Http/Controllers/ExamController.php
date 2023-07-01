6898066bb670f895842eda44e5f780f60bb95a69
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-07-01 17:09:54 +0800

    feat: shuffle question and limit to 50

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -85,7 +85,7 @@ class ExamController extends Controller
                 'finished' => false,
             ]);
 
-            foreach ($exercise->questions as $question) {
+            foreach ($exercise->questions->shuffle()->take(50) as $question) {
                 ExamAnswer::create([
                     'exam_id' => $exam->id,
                     'question_id' => $question->id,
