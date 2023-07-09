4ab8fff1ae5e9b8a9863ebf6885a68efcb496f03
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-07-09 15:34:05 +0800

    fix(migration): drop column number_of_question in down

--- database/migrations/2023_07_01_091045_add_columns_to_exercise_questions_table.php
+++ database/migrations/2023_07_01_091045_add_columns_to_exercise_questions_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('exercise_questions', function (Blueprint $table) {
-            //
+            $table->dropColumn("number_of_question");
         });
     }
 };
