08fad3d366e0590d9668e8be3dc420b197d35309
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-08-25 06:03:49 +0800

    fix: reapply exam cluster

---
+++ database/migrations/2023_08_24_215227_reapply_cluster_in_exams_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Exam;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+
+        \DB::transaction(function () {
+            $exams = Exam::with(['exerciseQuestion'])->get();
+
+            foreach ($exams as $exam) {
+                $cluster_names = $exam->exerciseQuestion->cluster_names;
+                $cluster = collect($cluster_names)->map(fn ($name, $key) => ['counter' => $exam->counter[$key] ?? 0, 'name' => $name]);
+                $mapped_cluster_names = collect($cluster_names)->mapWithKeys(fn ($name, $key) => [$name => $key]);
+
+                $cluster = $cluster->mapWithKeys(fn ($c) => [$mapped_cluster_names->get($c['name']) => $c]);
+                $exam->update([
+                    'cluster' => $cluster
+                ]);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    }
+};
