ebba2989bf249a1a42a86864ce4ea0efa17ab5cf
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-08-30 10:42:51 +0800

    feat: only allow one session per user

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,10 +6,13 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
@@ -48,6 +51,42 @@ class FortifyServiceProvider extends ServiceProvider
             return Limit::perMinute(5)->by(
                 $request->session()->get('login.id'),
             );
+        });
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)->first();
+
+            $sessions = \DB::connection(config('session.connection'))
+                ->table(config('session.table', 'sessions'))
+                ->where('user_id', $user->getAuthIdentifier())
+                ->orderBy('last_activity', 'desc')
+                ->first();
+
+
+            $isLoggedIn =
+                $user &&
+                Hash::check($request->password, $user->password);
+
+            if (!$isLoggedIn) {
+                return false;
+            }
+
+            // only allow one session per user
+            if ($sessions) {
+                $last_activity = \Carbon\Carbon::parse($sessions->last_activity);
+
+                if ($last_activity < \Carbon\Carbon::now()->subMinutes(5)) {
+                    throw ValidationException::withMessages([
+                        Fortify::username() => 'Sudah ada user yang login menggunakan akun ini'
+                    ]);
+                } else {
+                    \DB::connection(config('session.connection'))
+                        ->table(config('session.table', 'sessions'))
+                        ->delete($sessions->id);
+                }
+            }
+
+            return $user;
         });
     }
 }
