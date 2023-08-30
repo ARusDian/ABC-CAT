3860f6375aefd4600dfb23a3ba3f19b65e6d2fad
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-08-30 18:48:23 +0800

    feat: default randomize_choice to true for all type

--- app/Http/Controllers/ExerciseQuestionController.php
+++ app/Http/Controllers/ExerciseQuestionController.php
@@ -80,15 +80,15 @@ class ExerciseQuestionController extends Controller
             'type' => $data['type'],
             'time_limit' => $data['time_limit'],
             'options' => [
+                'randomize_choice' => true,
                 'time_limit_per_cluster' => $isKecermatan,
                 'number_of_question_per_cluster' => $isKecermatan,
                 'next_question_after_answer' => $isKecermatan,
-                'randomize_choice' => $isKecermatan,
 
                 ...($isKecermatan ? [
                     // cluster prefix will only be used if cluster_by_bank_question is false
                     'cluster_name_prefix' => 'Kolom'
-                    
+
                 ] : [
                     'cluster_name_prefix' => null
                 ]),
