d29cfbf40842cded5453e99e2e369d35d2747f06
RUVNE <dian.galleon@gmail.com>
2023-10-19 13:12:41 +0800

    fix: change export button to open another tab

--- resources/js/Layouts/Admin/DashboardAdminLayout.tsx
+++ resources/js/Layouts/Admin/DashboardAdminLayout.tsx
@@ -231,26 +231,27 @@ export default function DashboardAdminLayout({
               </ResponsiveNavLink>
             </li>
             <li>
-              <ResponsiveNavLink
+              <a
                 href={route('dev.dump-db')}
-                active={route().current('dev.dump-db')}
+                target='_blank'
+                className='block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition'
               >
                 <span className={'mr-4'}>
                   <FolderSharedIcon fontSize="large" />
                 </span>
                 dumpDB
-              </ResponsiveNavLink>
+              </a>
             </li>
             <li>
-              <ResponsiveNavLink
+              <a
                 href={route('dev.dump-redis')}
-                active={route().current('dev.dump-redis')}
+                className='block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition'
               >
                 <span className={'mr-4'}>
                   <FolderSharedIcon fontSize="large" />
                 </span>
                 dumpRedis
-              </ResponsiveNavLink>
+              </a>
             </li>
           </>
         )}
